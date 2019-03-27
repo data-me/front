8a3b1137e5ad6bd370de77ba6e9450c81bf03acf
carlosevi94 <carlosevillabarcelo@gmail.com>
2019-03-27 23:33:20 +0100

    Working on CV

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,6 +11,7 @@
         <b-navbar-nav class="ml-auto">
             <b-nav-item href="/explore">Explore</b-nav-item>
             <b-nav-item href="#">Pricing</b-nav-item>
+            <b-nav-item href="/my_cv">My Curriculum</b-nav-item>
             <b-nav-item href="/login">Log In</b-nav-item>
             <b-nav-item-dropdown text="Lang" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
---
+++ src/pages/cv/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <Navbar/>
+        
+
+          <div id="cv" v-for="item in items">
+          <b-card :title="item.name" :sub-title="item.description  ">
+            
+            
+          </b-card>
+        </div>
+
+      
+
+  </div>
+</template>
+
+<script>
+import Navbar from '../../components/Navbar.vue'
+import Footer from '../../components/Footer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+      items: [],
+    }
+  }, mounted: function () {
+    var token = 'JWT ' + this.$cookies.get('token')
+    this.$http.get('http://localhost:8000/api/v1/cv',{ headers: 
+      { Authorization: token }
+      }).then((result) => {
+        this.items = result.data
+      })
+  }
+}
+
+/*{
+          'title': this.form.title,
+          'description': this.form.description,
+          'price_offered': this.form.price_offered,
+          'currency': '0',
+          'limit_time': '2019,12,12,10,40,0,0'
+      },{ headers: {
+       'Content-Type': 'multipart/form-data'
+      }*/
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  
+}
+
+#offers {
+  margin: 2em;
+}
+
+.create-offer {
+  text-align: right;
+}
+
+#create-offer {
+  margin-top: 2em;
+  margin-right: 2em;
+}
+
+html {
+  background-color: #ffffff;
+}
+
+</style>
