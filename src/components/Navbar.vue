e4968c395a9a6a35f06aef8d434f44d59eae4cec
Agustín Núñez <seviagus@gmail.com>
2019-03-28 01:51:56 +0100

    List companies

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,6 +10,7 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
             <b-nav-item href="/explore">Explore</b-nav-item>
+            <b-nav-item href="/companies">Companies</b-nav-item>
             <b-nav-item href="#">Pricing</b-nav-item>
             <b-nav-item href="/login">Log In</b-nav-item>
             <b-nav-item-dropdown text="Lang" right>
